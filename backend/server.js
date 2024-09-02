4ef96be42acce386123ab746e14303c91893242a
Devansh27112001 <dkansara2711@gmail.com>
2024-09-02 18:32:54 -0400

    Initial commit and database setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./lib/db.js";
+
+import authRouter from "./routes/auth.route.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRouter);
+app.listen(PORT, () => {
+  console.log("Listening on http://localhost:" + PORT);
+  connectDb();
+});
