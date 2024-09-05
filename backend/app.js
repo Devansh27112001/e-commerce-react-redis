793d69c36cee84cf21003d0aa0d29c75e4fbf3b3
Devansh27112001 <dkansara2711@gmail.com>
2024-09-05 14:59:10 -0400

    Implemeted routes for cart and controllers for the cartRoutes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import productRouter from "./routes/product.route.js";
+import cartRouter from "./routes/cart.route.js";
 const app = express();
 // Middlewares
 app.use(morgan("dev"));
@@ -12,5 +13,6 @@ app.use(cookieParser()); // for parsing body of the request
 // Routes
 app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
+app.use("/api/cart", cartRouter);
 
 export default app;
