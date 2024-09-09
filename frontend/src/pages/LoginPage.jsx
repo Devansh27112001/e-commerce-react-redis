c40af7dceecdbb522b7439ca66f28f8559849b85
Devansh27112001 <dkansara2711@gmail.com>
2024-09-09 12:01:09 -0400

    FRONTEND: completed the login page UI

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,119 @@
-import React from "react";
-
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const isLoading = false;
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+    setCredentials({ email: "", password: "" });
+  };
+  return (
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2 className="text-center mt-6 text-3xl font-extrabold text-emerald-400">
+          Log in to your account
+        </h2>
+      </motion.div>
+      <motion.div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium shadow-sm"
+              >
+                Email address
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </div>
+                <input
+                  id="email"
+                  type="text"
+                  required
+                  value={credentials.email}
+                  onChange={(e) =>
+                    setCredentials({ ...credentials, email: e.target.value })
+                  }
+                  className="block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
+                  placeholder="youremail@example.com"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium shadow-sm"
+              >
+                Password
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                </div>
+                <input
+                  id="password"
+                  type="password"
+                  required
+                  value={credentials.password}
+                  onChange={(e) =>
+                    setCredentials({ ...credentials, password: e.target.value })
+                  }
+                  className="block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
+                  placeholder="********"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <Loader
+                  className="mr-2 w-5 h-5 animate-spin"
+                  aria-hidden="true"
+                >
+                  Loading...
+                </Loader>
+              ) : (
+                <>
+                  <LogIn className="mr-2 w-5 h-5" aria-hidden="true" />
+                  Log In
+                </>
+              )}
+            </button>
+          </form>
+          <p className="mt-8 text-center text-sm text-gray-400">
+            Not a member?{" "}
+            <Link
+              to="/signup"
+              className="font-medium text-emerald-600 hover:text-emerald-500 transition duration-150 ease-in-out"
+            >
+              Sign up now <ArrowRight className="h-4 w-4 inline" />
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default LoginPage;
