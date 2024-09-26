c5dbb036f4055b6938fd419797392d2fa2892938
Devansh27112001 <dkansara2711@gmail.com>
2024-09-26 14:18:41 -0400

    Project completed

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-
+import path from "path";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import productRouter from "./routes/product.route.js";
@@ -9,6 +9,8 @@ import couponRouter from "./routes/coupon.route.js";
 import paymentRouter from "./routes/payment.route.js";
 import analyticsRouter from "./routes/analytic.route.js";
 const app = express();
+const __dirname = path.resolve();
+
 // Middlewares
 app.use(morgan("dev"));
 app.use(
@@ -25,5 +27,13 @@ app.use("/api/cart", cartRouter);
 app.use("/api/coupons", couponRouter);
 app.use("/api/payments", paymentRouter);
 app.use("/api/analytics", analyticsRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  // If the user visits any other route other than "/", then serve the index.html file from the frontend folder.
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 export default app;
